520ae1a5e8825c48ce0da7b081ca686d23c663eb
kritijain13 <kritijain1308@gmail.com>
2026-03-19 11:35:34 +0530

    Add pattern printing examples in patternquestions.py

---
+++ list,patternquestions.py
@@ -0,0 +1,948 @@
+# n=int(input('enter a num'))
+# for i in range(1,5):
+#     for j in range(1,i):
+#         print("_",end=' ')
+#     for k in range(1,5):
+#         print('*',end=' ')
+#     print()
+    
+
+# n=int(input('enter a num'))
+# for i in range(1,n+1):
+#     for j in range(1,n+1):
+#         if i==n or j==1 or j==n or i==1 :
+#          print('*',end=' ')
+#         else:
+#            print(" ",end=' ')
+#     print()
+
+
+
+
+# for i in range(1,5):
+#     for j in range(1,i+1):
+#         print(chr(64+j),end=' ')
+#     print()
+
+
+
+# for i in range(1,5):
+#     for j in range(1,i+1):
+#         print(chr(64+i),end=' ')
+#     print()
+
+
+# for i in range(1,5):
+#     for j in range(1,i+1):
+#         print(chr(69+j-i),end=' ')
+#     print()
+
+
+# for i in range(1,6):
+#     for j in range(6-i):
+#         print("_",end=' ')
+#     for k in range(1,i+1):
+#         print("*",end=' ')
+#     print()
+
+
+
+# for i in range(1,6):
+#     x=1
+#     for j in range(6-i):
+#         print(" ",end=' ')
+#     for k in range(1,i+1):
+#         print(x,end=' ')
+#         x+=1
+#     print()
+
+
+
+# for i in range(6,0,-1):
+#     x=6
+#     for j in range(6-i):
+#         print(" ",end=' ')
+#     for k in range(1,i+1):
+#         print(x,end=' ')
+#         x-=1
+#     print()
+
+
+
+# for i in range(1,5):
+#     for j in range(5-i):
+#         print(" ",end=' ')
+#     for k in range(1,5):
+#         print("*",end=' ')
+#     print()
+
+
+# for i in range(1,5):
+#     for j in range(5-i):
+#         print(" ",end=' ')
+#     for k in range(1,5):
+#         print(k,end=' ')
+#     print()
+
+
+# for i in range(1,5):
+#     for j in range(5-i):
+#         print(" ",end=' ')
+#     for k in range(1,5):
+#         print(chr(64+k),end=' ')
+#     print()
+
+
+
+# for i in range(1,5):
+#     for j in range(5-i):
+#         print(" ",end=' ')
+#     for k in range(2*i-1):
+#         print('*',end=' ')
+#     print()
+
+
+
+# for i in range(1,5):
+#     for k in range(5-i):
+#         print("_",end=' ')
+#     for j in range(1,2*i):
+#         if j==1 or i==4 or j==2*i-1:
+#             print("*",end=' ')
+#         else:
+#             print(' ',end=' ')
+#     print()
+
+
+
+
+# for i in range(5,0,-1):
+#     for j in range(5-i):
+#         print(" ",end=' ')
+#     for k in range(2*i-1):
+#         print('*',end=' ')
+#     print()
+
+
+
+
+# for i in range(1,5):
+#     for j in range(5-i):
+#         print(" ",end=' ')
+#     for k in range(1,i+1):
+#         print('*',end=' ')
+#     print()
+# for i in range(5,0,-1):
+#     for j in range(5-i):
+#         print(' ',end=' ')
+#     for k in range(1,i+1):
+#         print('*',end=' ')
+#     print()
+
+
+
+
+# for i in range(1,5):
+#     for j in range(5-i):
+#         print(" ",end=' ')
+#     for k in range(2*i-1):
+#         print('*',end=' ')
+#     print()
+# for i in range(5,0,-1):
+#     for j in range(5-i):
+#         print(" ",end=' ')
+#     for k in range(2*i-1):
+#         print('*',end=' ')
+#     print()
+
+
+
+
+
+
+# for i in range(1,5):
+#     for j in range(1,i+1):
+#             print(j,end=' ')
+#     print()
+
+
+
+
+# for i in range(5,0,-1):
+#     for j in range(1,i+1):
+#         print(j,end=' ')
+#     print()
+
+
+# for i in range(1,5):
+#     for j in range(1,i+1):
+#         if j==1 or i==4 or j==i:
+#             print(j,end=' ')
+#         else:
+#             print(' ',end=' ')
+#     print()
+
+
+
+# for i in range(1,5):
+#     for j in range(5-i):
+#         print(' ',end=' ')
+#     for k in range(1,2*i):
+#         if i==4 or  k==1 or k==2*i-1:
+#             print(k,end=' ')
+#         else:
+#             print(" ",end=' ')
+#     print()
+
+
+
+
+# for i in range(5,0,-1):
+#     for j in range(1,i+1):
+#         if i==5 or j==i or j==1:
+#             print(j,end=' ')
+#         else:
+#             print(' ',end=' ')
+#     print()
+
+
+
+
+# for i in range(1,5):
+#     for j in range(5-i):
+#         print(" ",end=' ')
+#     for k in range(2*i-1):
+#         print('*',end=' ')
+#     print()
+# for i in range(5,0,-1):
+#     for j in range(5-i):
+#         print(" ",end=' ')
+#     for k in range(2*i-1):
+#         print('*',end=' ')
+#     print()
+
+
+
+# for i in range(5,0,-1):
+#     for j in range(5-i):
+#         print(" ",end=' ')
+#     for k in range(2*i-1):
+#         print('*',end=' ')
+#     print()
+# for i in range(2,6):
+#     for j in range(5-i):
+#         print(' ',end=' ')
+#     for k in range(2*i-1):
+#         print('*',end=' ')
+#     print()
+
+
+
+# for i in range(1,5):
+#     for j in range(1,5):
+#         if j==4 or i==4 or j==1 or i==1:
+#             print(chr(64+j),end=' ')
+#         else:
+#             print(" ",end=' ')
+#     print()
+
+
+# for i in range(1,5):
+#     for j in range(5-i):
+#         print(' ',end=' ')
+#     for k in range(1,2*i):
+#         if k==1 or k==2*i-1:
+#             print("*",end=' ')
+#         else:
+#             print(' ',end=' ')
+#     print()
+# for i in range(5,0,-1):
+#     for j in range(5-i):
+#         print(" ",end=' ')
+#     for k in range(1,2*i):
+#         if k==1 or k==2*i-1:
+#             print("*",end=' ')
+#         else:
+#             print(" ",end=' ')
+#     print()
+
+
+
+# for i in range(5):
+#     x=1
+#     for j in range(i+1):
+#         print(x,end=' ')
+#         x=x*(i-j)//(j+1)
+#     print()
+
+
+
+# ch=65
+# x=1
+# for i in range(5,0,-1):
+#     for j in range(1,i+1):
+#         print(chr(ch),end=' ')
+#         print(x,end=' ')
+#         ch+=1
+#         x+=1
+#     print()
+
+
+
+# for i in range(1,5):
+#     for j in range(1,i+1):
+#         if((i+j)%2==0):
+#             print('1',end=' ')
+#         else:
+#             print('0',end=' ')
+#     print()
+
+
+
+# * * *
+# * *
+# * * *
+for i in range(3):
+    if i==1:
+     for j in range(2):
+      print('*',end=' ')
+    else:
+      for i in range(3):
+        print('*',end=' ')
+    print()
+
+
+# for i in range(5,0,-1):
+#   x=5
+#   for k in range(5-i):
+#     print(" ",end=' ')
+#   for j in range(1,i+1):
+#     print(x,end=' ')
+#     x-=1
+#   print()
+
+
+# x=1
+# for i in range(5,0,-1):
+  
+#   for j in range(1,i+1):
+#     print(x+j-1,end=' ')
+#   x+=2
+#   print()
+
+# for i in range(1,5):
+#     print('*'*i,end=' ')
+#     print('*'*(2*(5-i)),end=' ')
+#     print('*'*i)
+# for i in range(5,0,-1):
+#     print('*'*i,end=' ')
+#     print('*'*(2*(5-i)),end=' ')
+#     print('*'*i)
+
+
+# s='this is me'
+# print(s.replace('is','kriti'))
+
+# s=input('enter a num')
+# for i in range(len(s)):
+#     print(i)
+
+
+# sen=input('enter a num')
+# vowel='aeiouAEIOU'
+# count=0
+# word=sen.split()
+# for i in word:
+#     if i[0] in vowel and i[-1] in vowel:
+#         print('strt with vowel',i)
+#         count+=1
+# print(count)
+
+
+# s=input('enter a string')
+# sum=0
+# for i in s:
+#         if i.isdigit():
+#          sum+=int(i)
+# print(sum)
+
+# s=input('enter a string')
+# sen=s.split()
+# for i in sen:
+#     if i==i[::-1]:
+#         print('palindrom')
+#     else:
+#         print('not')
+
+
+# s=input('enter a string')
+# rev=''
+# for i in s:
+#     rev=i+rev
+# # print(rev)
+# s=input('enter a string')
+# print(s[::-1])
+
+# s=input('enter a string')
+# if s[0]==s[-1]:
+#     print('same')
+# else:
+#     print('not')
+
+# s=input('enter a string')
+# for i in s:
+#     if i.isdigit():
+#         n=int(i)
+#         print('square is',n**2)
+#     else:
+#         print('invalid')
+# s=input('enter a string')
+# vowel='aeiouAEIOU'
+# count=0
+# for i in s:
+#     if i in vowel:
+#         count+=1
+#         print(i)
+# print(count)
+# s=input('enter a string')
+# count=0
+# for i in s:
+#     if i.isdigit():
+#         count+=1
+# print(count)
+
+
+# for i in range(1,101):
+#     if i==37:
+        
+#         pass
+#     print(i)
+# a=0
+# b=1
+# for i in range(1,7):
+#     print(a)
+#     a,b=b,a+b
+
+# n=int(input('enter a num'))
+# is_prime=True
+# for i in range(2,n):
+#     if n%i==0:
+#         is_prime=False
+# if is_prime:
+#     print('prime')
+# else:
+#     print('not prime')
+
+
+# n=int(input('enter a num'))
+# rev=0
+# last=n%10
+# while n>=10:
+#     n=n//10
+# first=n
+# print(first,last)
+
+# n=int(input('enter a num'))
+# count=0
+# while n!=0:
+#     n=n//10
+#     count+=1
+# print(count)
+
+# n=int(input('enter a num'))
+# i=1
+# fact=1
+# while i<=n:
+#     fact=fact*i
+#     i=i+1
+# print(fact)
+# # 
+# n=int(input('enter a num'))
+# for i in range(1,n+1):
+#     if n%i==0:
+#         print(i)
+
+# n=int(input('enter a num'))
+# sum=0
+# for i in range(1,n):
+#     if n%i==0:
+#         sum=sum+i
+# print(sum)
+# if sum==n:
+#     print('perfect num')
+# else:
+#     print('not')
+
+# n[p
+# a=int(input('enter a num'))
+# b=int(input('enter a num'))
+# i=1
+# while a>=i or b>=i:
+#     if a%i==0 or b%i==0:
+#         print(i)
+#     i+=1
+# for i in range(2,101):
+#     if int(i**0.5)**2==i:
+#         print(i)
+# sum=0
+# for i in range(1,101):
+#     if i%5==0:
+#         continue
+#     sum=sum+i
+# print(sum)
+
+# a='kriti'
+# index=0
+# while index<6:
+#     print(index,a[index],a)
+#     index+=1
+# n=input('enter a num')
+# while len(n):
+#     print(n[0],n[-1])
+#     break
+# a=int(input('enter a num'))
+# b=int(input('enter a num'))
+# power=1
+# while b>=1:
+#     power=power*a
+#     b=b-1
+# print(power)
+# ch=65
+# while ch<=90:
+#     print(chr(ch),end=' ')
+#     ch+=1
+
+# s='kriti'
+# index=0
+# last=len(s)-1
+# while index<len(s):
+#     print(last,s[last],s[index],index)
+#     index+=1
+#     last-=1
+
+# s=input('entyer a string')
+# strt=0
+# end=len(s)-1
+# a=True
+# while strt<end:
+#     if s[strt]!=s[end]:
+#         a=False
+#         break
+#     strt+=1
+#     end-=1
+# if a:
+#     print('palindrom')
+# else:
+#     print('not')
+# res=[i for i in range(1,10) if i%2==0]
+# print(res)
+# numbers = [1, 2, 3, 2, 4, 5, 1, 6]
+# remove=[]
+# for i in numbers:
+#     if i not in remove:
+#         remove.append(i)
+# print(remove)
+# numbers = [4, 5, 1, 2, 0, 4, 5, 2]
+# unoque=[]
+# for i in numbers:
+#     if i not in unoque:
+#         unoque.append(i)
+#     else:
+#         unoque.remove(i)
+# print(unoque)
+
+# nested_list = [1, [2, 3], [4, [5, 6], 7], 8]
+# list2=[]
+# for i in nested_list:
+#     if type(i)==list:
+#         for j in i:
+#             if type(j)==list:
+#                 for k in j:
+# #                     list2.append(k)
+# #             else:
+# #                 list2.append(j)
+# #     else:
+# #         list2.append(i)
+# # print(list2)
+#     #17. Find Elements Greater Than Their Left Neighbor
+# # numbers = [1, 3, 2, 6, 5, 8, 7]
+# # res=[]
+# # for i in range(len(numbers)):
+# #     if numbers[i]>numbers[i-1]:
+# #         res.append(numbers[i])
+# # print(res)
+# # mydict={'apple':1,'banana':2,'kiwi':3}
+# # for i in mydict:
+# #     print(i,mydict[i])
+# # data='abca'
+# # d={}
+# # for i in range(len(data)):
+# #     s=''
+# #     for j in range(i,4):
+# #         s=s+data[j]
+# #         if j not in d:
+# #             d[j]=1
+# #         else:
+# #             d[j]+=1
+# #     print(data[j],i,j,d[j],s)
+# # myset={1,2,3}
+# # myset.discard(4)
+# # print(myset)
+
+
+# # list1=[1,[2,3],4,[5,6,7],8,[9]]
+# # list2=[]
+# # for i in list1:
+# #     if type(i)==list:
+# #         for j in i:
+# #             if type(j)==list:
+# #                 for k in j:
+# #                     list2.append(k)
+# #             else:
+# #                 list2.append(j)
+# #     else:
+# #         list2.append(i)
+# # print(list2)
+
+# num=1
+# for i in range(1,5):
+#     for j in range(5-i):
+#         print(' ',end=' ')
+#     for k in range(1,2*i):
+#         if i==4 or k==1 or k==2*i-1:
+#             print(num,end=' ')
+#             num+=1
+#         else:
+#             print(' ',end=' ')
+#     print()
+
+
+
+     
+
+# for i in range(1,5):
+#     for j in range(1,5-i+1):
+#         print(' ',end='')
+#     for k in range(1,i+1):
+#         print('*',end=' ')
+
+
+# data = [(3, 'banana'), (1, 'apple'), (2, 'mango')]
+# for i in range(len(data)):
+#     for j in range(i+1):
+#        if  data[i][0]<data[j][0]:
+#            data[i],data[j]=data[j],data[i]
+# print(data)
+
+
+# def printer_error(s):
+#     error=0
+#     for i in s:
+#         if i<'a' or i>'m':
+#             error+=1
+#     return (f'{error}/{len(s)}')
+# x=printer_error('aaaxbbbbyyhwawiwjjjwwm')
+# print(x)
+            
+
+# nums=[2,7,11,15]
+# start=0
+# end=len(nums)-1
+# res=[]
+# while start<end:
+#     if(nums[start]+nums[end])==9:
+#         res.extend((nums[start],nums[end]))
+
+#     end-=1
+#     if start==end:
+#         start+=1
+#         end=len(nums)-1
+# print(res)
+
+
+# n=int(input('enter a num'))
+# fact=1
+# while 1<=n:
+#     fact=fact*n
+#     n=n-1
+# print(fact)
+
+# n=int(input('enter a num'))
+# a=0
+# b=1
+# while n<=10:
+#     print(a)
+#     a,b=b,a+b
+#     n+=1
+
+
+# i=947
+# count=0
+# while count<=15:
+#      p=2
+#      is_prime=True
+#      while p<i:
+#       if i%p==0:
+#         is_prime=False
+#         break
+#       p+=1
+    
+#      if is_prime:
+#        print(i)
+#        count+=1
+#      i+=1
+
+# li=[1,2,3,4,5]
+# li1=[3,4,5,6,7]
+# res=[]
+# unique=[]
+# for i in li:
+#     if i in li1:
+#         res.append(i)
+#     else:
+#         unique.append(i)
+# print(res)
+# print("uniw",unique)
+    
+# list=[1,2,3,4,5]
+# res=[]
+# for i in list:
+#     if i%2==0:
+#         res.append(i)
+# print(res)
+
+
+# list=[1,2,1,2,3,4]
+# res=[]
+# for i in list:
+#     if i not in res:
+#         res.append(i)
+# print(res)
+
+
+# a=[3,1,2,3,4,5]
+# max=a[0]
+# mini=a[0]
+# for i in a:
+#     if(i>max):
+#         max=i
+#     elif(mini>i):
+#         mini=i
+# print(max)
+# print(mini)
+
+# lsit1=[1,2,2,3,4,6,5,5]
+# secondlargest=lsit1[0]
+# max=lsit1[0]
+# for i in lsit1:
+#     if(i>max):
+#         secondlargest=max
+#         max=i
+# print(secondlargest)
+
+# lsit1=[1,2,2,3,4,6,5,5,6]
+# secondlargest=lsit1[0]
+# max=lsit1[0]
+# for i in lsit1:
+#     if(i>max):
+#         secondlargest=max
+#         max=i
+#     elif(i!=max and i>secondlargest):
+#         secondlargest=i
+# print(secondlargest)
+
+
+
+# s='this is a sentance'
+# sen=s.split()
+# list=[]
+# for i in sen:
+#      list.append(i[::-1])
+# print(list)
+
+# a = [1, 2, 3, 4, 5]
+# k = 2
+
+# for i in range(k):
+#     x = a.pop()        # remove last element
+#     a.insert(0, x)    # add at beginning
+
+# print(a)
+
+# li=[1,2,3,4,5]
+# k=2
+# left=0
+# right=len(li)-1
+# while left<right:
+#     li[left],li[right]=li[right],li[left]
+#     left+=1
+#     right-=1
+# print(li)
+# left=0
+# right=k-1
+# while left<right:
+#     li[left],li[right]=li[right],li[left]
+#     left+=1
+#     right-=1
+# print(li)
+# left=k
+# right=len(li)-1
+# while left<right:
+#     li[left],li[right]=li[right],li[left]
+#     left+=1
+#     right-=1
+# print(li)
+
+
+
+
+# li=[1,2,3,4,5]
+# strt=0
+# end=len(li)-1
+# newlist=[]
+# while strt<end:
+#     if li[strt]+li[end]==7:
+#        newlist.extend([li[strt],li[end]])   
+#        print(strt,end)   
+#     end-=1
+#     if end==strt:
+#         strt+=1
+#         end=len(li)-1
+
+
+
+
+
+
+# li=[1,-2,3,-4,5]
+# prod=1
+# for i in (li):
+#     if i>0:
+#         prod=prod*i
+# print(prod)
+
+
+
+# li=[[1,2,3],[3,4,5]]
+# newlist=[]
+# for j in range(len(li[0])):
+#     for i in range(len(li)):
+#         newlist.append(li[i][j])
+# print(newlist)
+
+
+
+
+
+# li=[[1,2,3],[3,4,5]]
+# sum=0
+# for i in li:
+#     for j in i:
+#         sum+=j
+# print(sum)
+
+
+
+
+
+# li=[[1,2,3],4,[5,6]]
+# flatten_li=[]
+# for i in range(len(li)):
+#      for j in range(len(li[i])):
+#         flatten_li.append(li[i][j])
+# print(flatten_li)
+
+
+# li=[[1,2,3],[4,5,6],[7,8,9]]
+# for i in  range(len(li)):
+#     for j in range(len(li[i])):
+#         if i==j:
+#             print(li[i][i])
+
+
+# li=[[1,2,3],[4,5,6],[7,8,9]]
+# sum=0
+# for i in range(len(li)):
+#     for j in range(len(li[i])):
+#         if i+j==2:
+#             sum+=li[i][j]
+# print(sum)
+
+
+
+
+# li=[[1,2,3],[4,5,6]]
+# maxium=li[0][0]
+# for i in li:
+#     for j in i:
+#      if j>maxium:
+#         maxium=j
+# print(maxium)
+
+
+
+#even
+# res=[i for i in range(1,101) if i%2==0]
+# print(res)
+
+#odd
+# res=[i for i in range(1,101) if i%2!=0]
+# print(res)
+
+
+
+
+
+# res=[i*i for i in range(1,11)]
+# print(res)
+
+
+# res=['apple','car','elephant','dog','cat']
+# for i in res:
+#     if len(i)<4:
+#         print(i)
+
+
+
+# res=['apple','car','elephant','dog','cat']
+# res=[i for i in res if len(i)<4]
+# print(res)
+
+
+
+
+# res=[1,2,3,4,5,6,7,8,10,12,15,17]
+# list=[i for i in res if i>10]
+# print(list)
+
+
+
+
+# list=[i for i in range(100,0,-1) if i%2!=0]
+# print(list)
+
+
+
+#implement deep copy and shallow copy
+# import copy
+# list=[[2,3,4],[3,4,5],1,2]
+# deep=copy.deepcopy(list)
+# print('befior change',id(deep),list)
+# list[0][0]=1
+# print('after change',id(deep),list)
+
+
+# list=['apple','cat','kiwi','banana']
+# res=[i.upper() for i in list ]
+# print(res)
+
+
+
+
+x=10
+if x>5:
+    print('a')
+elif x>8:
+    print('b')
+else:
+    print('c')
